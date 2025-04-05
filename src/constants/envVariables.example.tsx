b6a737e2656bbf20e44a9af3d0dcf69dd5a9beba
baariq <baariqazhar@gmail.com>
2025-04-05 20:51:38 +0700

    fix: fix env, change favicon

--- src/constants/envVariables.example.tsx
+++
@@ -1,4 +0,0 @@
-export const name = "name";
-export const linkLinkedin = "https://www.linkedin.com/in/example/";
-export const linkGithub = "https://github.com/example";
-export const linkInstagram = "https://www.instagram.com/example/";
---
+++ src/constants/envVariables.tsx
@@ -0,0 +1,4 @@
+export const name = process.env.NEXT_PUBLIC_NAME;
+export const linkLinkedin = process.env.NEXT_PUBLIC_LINK_LINKEDIN;
+export const linkGithub = process.env.NEXT_PUBLIC_LINK_GITHUB;
+export const linkInstagram = process.env.NEXT_PUBLIC_LINK_INSTAGRAM;
